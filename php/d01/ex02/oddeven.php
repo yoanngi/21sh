1443f6b9fa8c87481845c45c97c7dceb3080e91c
Yoann Ginet <yoginet@z3r6p3.le-101.fr>
2018-09-11 15:57:42 +0200

    Backup

---
+++ php/d01/ex02/oddeven.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+
+<?php
+	echo "Entrez un nombre: ";
+	$line = fgets(STDIN);
+	if (is_int($line) == true) {
+		echo $line . " est un nombre";
+	}
+	else {
+		echo $line . " n'est pas un nombre";
+	}
+?>
