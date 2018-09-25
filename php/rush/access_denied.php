3cbcfd4aec44f9d9e9a2e3c56f023b6ee0ab2f29
Yoann GINET <yoannginet@MacBook-Pro-de-Yoann.local>
2018-09-25 16:10:46 +0200

    clean

--- php/rush/access_denied.php
+++
@@ -1,5 +0,0 @@
-<?php
-    header("HTTP/1.1 401 Unauthorized ");
-    echo "<html><body>Accès refusé</body></html>";
-    header("refresh:3;url=index.php");
-?>
--- php/rush/admin/footeradmin.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" type="text/css" href="../css/rush00.css">
-<?php
-if (!isset($_SESSION['login']))
-    header('Location: ./access_denied.php');
-?>
-<div>
-    <p>Footer</p>
-</div>
