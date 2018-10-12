b88980a1e7da94bd9b3a8ce6d87bb9b6e1447ae3
Yoann Ginet <yoginet@z4r5p4.le-101.fr>
2018-10-12 10:29:00 +0200

    correction redirection

--- srcs/core/exec_child.c
+++ srcs/core/exec_child.c
@@ -6,7 +6,7 @@
 /*   By: yoginet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/08 12:53:56 by yoginet      #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/11 14:20:21 by yoginet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/12 10:28:42 by yoginet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,11 +15,12 @@
 
 static int		exec_pipe_child2(t_cmd *lst, int pipe_fd[2], int *fd_in)
 {
+	redirection_fd(lst);
 	if (lst->pathname != NULL)
 		return (fork_redirection(lst));
-	dup2(*fd_in, lst->stdin_cmd) == -1 ? basic_error("dup2", "3 failled") : 0;
-	close(pipe_fd[1]) == -1 ? basic_error("close", "1 failled") : 0;
-	close(pipe_fd[0]) == -1 ? basic_error("close", "2 failled") : 0;
+	dup2(*fd_in, lst->stdin_cmd) == -1 ? basic_error("dup2", " failled") : 0;
+	close(pipe_fd[1]) == -1 ? basic_error("close", " failled") : 0;
+	close(pipe_fd[0]) == -1 ? basic_error("close", " failled") : 0;
 	return (execve(lst->rep, lst->tab_cmd, lst->env));
 }
 
@@ -41,8 +42,9 @@ static int		exec_pipe_op_1(t_cmd *lst, int pipe_fd[2], int *fd_in)
 {
 	dup2(*fd_in, lst->stdin_cmd) == -1 ? basic_error("dup2", "failled") : 0;
 	dup2(pipe_fd[1], lst->stdout_cmd) == -1 ?
-	basic_error("dup2", "2 failled") : 0;
-	close(pipe_fd[0]) == -1 ? basic_error("close", "3 failled") : 0;
+	basic_error("dup2", " failled") : 0;
+	redirection_fd(lst);
+	close(pipe_fd[0]) == -1 ? basic_error("close", " failled") : 0;
 	if (lst->pathname != NULL)
 		duplique_process(lst, pipe_fd, fd_in);
 	if (lst->redir_heredoc == 1)
@@ -55,7 +57,6 @@ int				exec_pipe_child(t_struct *mystruct, t_cmd *lst, int pipe_fd[2],
 {
 	int		builtins;
 
-	redirection_fd(lst);
 	if ((builtins = execute_builtins(mystruct, lst, pipe_fd, fd_in)) != -1)
 		return (builtins);
 	if (lst->op_next == 1)
